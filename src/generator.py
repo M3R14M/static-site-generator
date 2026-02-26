8115eab27f2f49494deb7751d7da6ad797acdd0a
M3R14M <git@meriam.dev>
2026-02-26 13:13:41 +0100

    add basepath parameter to page generation functions for dynamic path handling

--- src/generator.py
+++ src/generator.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
     raise Exception("No h1 header found in markdown")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, 'r') as f:
@@ -25,6 +25,8 @@ def generate_page(from_path, template_path, dest_path):
 
     full_html = template_content.replace("{{ Title }}", title)
     full_html = full_html.replace("{{ Content }}", html_content)
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
     
     dest_dir = os.path.dirname(dest_path)
     if dest_dir:
@@ -34,7 +36,7 @@ def generate_page(from_path, template_path, dest_path):
         f.write(full_html)
 
 
-def generate_pages_recursive(content_dir, template_path, dest_dir):
+def generate_pages_recursive(content_dir, template_path, dest_dir, basepath):
     items = os.listdir(content_dir)
     for item in items:
         src_path = os.path.join(content_dir, item)
@@ -42,7 +44,7 @@ def generate_pages_recursive(content_dir, template_path, dest_dir):
         if os.path.isfile(src_path):
             if src_path.endswith(".md"):
                 dest_html = os.path.splitext(dest_path)[0] + ".html"
-                generate_page(src_path, template_path, dest_html)
+                generate_page(src_path, template_path, dest_html, basepath)
             continue
         os.makedirs(dest_path, exist_ok=True)
-        generate_pages_recursive(src_path, template_path, dest_path)
+        generate_pages_recursive(src_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from copy_directory import *
 from generator import generate_pages_recursive
 import os
+import sys
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     script_dir = os.path.dirname(os.path.abspath(__file__))
     static_dir = os.path.join(os.path.dirname(script_dir), "static")
-    public_dir = os.path.join(os.path.dirname(script_dir), "public")
+    public_dir = os.path.join(os.path.dirname(script_dir), "docs")
     
     print("Starting directory copy...")
     copy_directory(static_dir, public_dir)
@@ -14,7 +20,7 @@ def main():
     content_path = os.path.join(os.path.dirname(script_dir), "content")
     template_path = os.path.join(os.path.dirname(script_dir), "template.html")
     
-    generate_pages_recursive(content_path, template_path, public_dir)
+    generate_pages_recursive(content_path, template_path, public_dir, basepath)
 
 if __name__ == '__main__':
     main()
